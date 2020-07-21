09ea942d37e862257c7d137a8c489b56b98f7fab
Qingsong Yao <qyao@internal.salesforce.com>
2020-07-20 19:36:16 -0700

    adding concurrentRequests config and section for how to dedupe

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -26,10 +26,14 @@ module.exports = function configJSON(req) {
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
         url: `https://${req.headers.host}/modules/discount-code/execute`,
-        // The amount of time we want Journey Builder to wait for our
+        // The amount of time we want Journey Builder to wait before cancel the request. Default is 60000, Minimal is 1000
         timeout: 10000,
+        // how many retrys if the request failed with 5xx error or network error. default is 0
         retryCount: 3,
-        retryDelay: 0
+        // wait in ms between retry.
+        retryDelay: 1000,
+        // The number of concurrent requests Journey Builder will send all together
+        concurrentRequests: 5
       }
     },
     configurationArguments: {
